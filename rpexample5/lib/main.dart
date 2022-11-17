2a338691456eead234c0bd58cf1b46588dc6bb8d
Tewodrosyeteji <teddytejinesh@gmail.com>
2022-11-17 13:51:23 +0300

    rpexample4&5

--- rpexample5/lib/main.dart
+++ rpexample5/lib/main.dart
@@ -1,115 +1,198 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:uuid/uuid.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  runApp(ProviderScope(
+    child: MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+      themeMode: ThemeMode.dark,
+      darkTheme: ThemeData.dark(),
+      home: const HomePage(),
+    ),
+  ));
+}
+
+@immutable
+class Person {
+  final String name;
+  final int age;
+  final String uuid;
+
+  Person({
+    required this.name,
+    required this.age,
+    String? uuid,
+  }) : uuid = uuid ?? const Uuid().v4();
+
+  Person update([String? name, int? age]) => Person(
+        name: name ?? this.name,
+        age: age ?? this.age,
+        uuid: uuid,
+      );
+
+  String get displayName => '$name($age years old)';
+
+  @override
+  bool operator ==(covariant Person other) => uuid == other.uuid;
+  @override
+  int get HashCode => uuid.hashCode;
+
+  @override
+  String toString() => 'Person(name=$name,age=$age,uuid=$uuid)';
+}
+
+class DataModel extends ChangeNotifier {
+  final List<Person> _person = [];
+
+  int get count => _person.length;
+
+  UnmodifiableListView<Person> get person => UnmodifiableListView(_person);
+
+  void add(Person person) {
+    _person.add(person);
+    notifyListeners();
+  }
+
+  void remove(Person person) {
+    _person.remove(person);
+    notifyListeners();
+  }
+
+  void update(Person updatedPerson) {
+    final index = _person.indexOf(updatedPerson);
+    final oldperson = _person[index];
+    if (oldperson.name != updatedPerson.name ||
+        oldperson.age != updatedPerson.age) {
+      _person[index] = oldperson.update(
+        updatedPerson.name,
+        updatedPerson.age,
+      );
+      notifyListeners();
+    }
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+final peopleProvider = ChangeNotifierProvider(
+  (_) => DataModel(),
+);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomePage extends ConsumerWidget {
+  const HomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Home page'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: Consumer(
+        builder: (context, ref, child) {
+          final dataModel = ref.watch(peopleProvider);
+          return ListView.builder(
+              itemCount: dataModel.count,
+              itemBuilder: ((context, index) {
+                final person = dataModel.person[index];
+                return ListTile(
+                  title: GestureDetector(
+                    onTap: () async {
+                      final updatePerson =
+                          await createOrUpdateDialog(context, person);
+                      if (updatePerson != null) {
+                        dataModel.update(updatePerson);
+                      }
+                    },
+                    child: Text(
+                      person.displayName,
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                );
+              }));
+        },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: () async {
+          final person = await createOrUpdateDialog(context);
+          if (person != null) {
+            final dataModel = ref.read(peopleProvider);
+            dataModel.add(person);
+          }
+        },
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
+}
+
+final nameController = TextEditingController();
+final ageController = TextEditingController();
+
+Future<Person?> createOrUpdateDialog(
+  BuildContext context, [
+  Person? existingPerson,
+]) {
+  String? name = existingPerson?.name;
+  int? age = existingPerson?.age;
+
+  nameController.text = name ?? '';
+  ageController.text = age?.toString() ?? '';
+
+  return showDialog<Person?>(
+      context: context,
+      builder: ((context) {
+        return AlertDialog(
+          title: Text('create a person'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                  labelText: 'Enter you name...',
+                ),
+                onChanged: (value) => name = value,
+              ),
+              TextField(
+                controller: ageController,
+                decoration: const InputDecoration(
+                  labelText: 'Enter you age...',
+                ),
+                onChanged: (value) => age = int.tryParse(value),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text('Close'),
+            ),
+            TextButton(
+              onPressed: () {
+                if (name != null && age != null) {
+                  if (existingPerson != null) {
+                    final newPerson = existingPerson.update(
+                      name,
+                      age,
+                    );
+                    Navigator.of(context).pop(newPerson);
+                  } else {
+                    Navigator.of(context).pop(
+                      Person(
+                        name: name!,
+                        age: age!,
+                      ),
+                    );
+                  }
+                } else {
+                  Navigator.of(context).pop();
+                }
+              },
+              child: const Text('Create'),
+            ),
+          ],
+        );
+      }));
 }
